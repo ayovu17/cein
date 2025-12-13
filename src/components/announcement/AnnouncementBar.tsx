5dd4c3c685e8e80ab00701b6291669617406d589
ayovu17 <ayovu17@gmail.com>
2025-12-13 06:23:05 +0400

    feat: add announcement bar

---
+++ src/components/announcement/AnnouncementBar.tsx
@@ -0,0 +1,26 @@
+import { Typography } from '@/components/ui'
+
+const mockData = {
+  mobile: 'The Archive Sale is live. Starting at 20% off. Show now',
+  desktop: 'Complimentary U.S. No-Rush Shipping on orders of $95 or more. Shop now',
+}
+
+export function AnnouncementBar() {
+  return (
+    <div
+      className="
+        flex justify-center items-center
+        w-full h-11
+        text-neutral-100
+        bg-neutral-900
+      "
+    >
+      <Typography variant="text-100" tag="p" className="md:hidden">
+        {mockData.mobile}
+      </Typography>
+      <Typography variant="text-100" tag="p" className="hidden md:block">
+        {mockData.desktop}
+      </Typography>
+    </div>
+  )
+}
