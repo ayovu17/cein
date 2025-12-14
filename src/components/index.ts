7e9d64bf49c6e78edd660c57a17ba084e22c251f
ayovu17 <ayovu17@gmail.com>
2025-12-14 20:54:40 +0400

    feat: add main section

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export * from './announcement/AnnouncementBar'
 export * from './header'
+export * from './sections'
 export * from './ui'
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,9 @@
+import { MainSection } from '@/components'
+
 export function HomePage() {
   return (
-    <></>
+    <>
+      <MainSection />
+    </>
   )
 }
