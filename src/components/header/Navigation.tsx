7253a010da2823311fc8af40a0430cfcc743cb3d
ayovu17 <ayovu17@gmail.com>
2025-12-21 10:53:19 +0400

    fix: add items-center to header components

--- src/components/header/Navigation.tsx
+++ src/components/header/Navigation.tsx
@@ -2,7 +2,7 @@ import { Button, LogoIcon } from '@/components'
 
 export function Navigation() {
   return (
-    <nav className="flex gap-10.5">
+    <nav className="flex items-center gap-10.5">
       <Button variant="icon" icon={LogoIcon} to="/" />
       <Button variant="text" size="sm" to="/">Shop</Button>
       <Button variant="text" size="sm" to="/">New Arrivals</Button>
