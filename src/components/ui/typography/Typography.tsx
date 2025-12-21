a3d7d67af3d005e321738703eed8ebebbb9151d9
ayovu17 <ayovu17@gmail.com>
2025-12-21 11:00:54 +0400

    feat(ui): add new variant, color to typography

--- src/components/ui/typography/Typography.tsx
+++ src/components/ui/typography/Typography.tsx
@@ -7,7 +7,8 @@ type TypographyVariant
     | 'text-400'
     | 'text-300'
     | 'text-100'
-type TypographyColor = 'white' | 'black'
+    | 'text-100cap'
+type TypographyColor = 'white' | 'black' | 'gray'
 type TypographyTag = 'h1' | 'h2' | 'div' | 'p' | 'span'
 
 type TypographyProps = React.ComponentProps<Exclude<TypographyTag, 'span'>> & {
@@ -32,10 +33,12 @@ export function Typography({
     variant === 'text-400' && 'text-(length:--text400) leading-(--leading400)',
     variant === 'text-300' && 'text-(length:--text300) leading-(--leading300)',
     variant === 'text-100' && 'text-(length:--text100) leading-(--leading100)',
+    variant === 'text-100cap' && 'text-(length:--text100cap) leading-(--leading100cap)',
   )
   const colorClass = clsx(
     color === 'white' && 'text-neutral-100',
     color === 'black' && 'text-neutral-900',
+    color === 'gray' && 'text-neutral-500',
   )
   const cn = clsx(
     variantClass,
