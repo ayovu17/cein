5d3a81d584120eda74abef7a9857764f037a3b83
ayovu17 <ayovu17@gmail.com>
2025-12-21 10:49:21 +0400

    feat(ui): add width to button

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -2,11 +2,13 @@ import { Link } from '@tanstack/react-router'
 import clsx from 'clsx'
 
 type ButtonVariant = 'contain' | 'text' | 'icon'
+type ButtonWidth = 'default' | 'full'
 type ButtonSize = 'xs' | 'sm' | 'md'
 type ButtonColor = 'white' | 'black' | 'gray'
 
 interface ButtonProps extends React.ComponentProps<'button'> {
   variant: ButtonVariant
+  width?: ButtonWidth
   size?: ButtonSize
   color?: ButtonColor
   icon?: React.ComponentType
@@ -17,6 +19,7 @@ interface ButtonProps extends React.ComponentProps<'button'> {
 export function Button({
   type = 'button',
   variant,
+  width = 'default',
   size = 'md',
   color = 'black',
   icon: Icon,
@@ -26,11 +29,16 @@ export function Button({
   ...props
 }: ButtonProps) {
   const variantClass = clsx(
+    (variant === 'contain' || variant === 'icon') && 'flex justify-center items-center',
     variant === 'contain' && color === 'white' && 'bg-neutral-100 text-neutral-700 hover:bg-white',
     variant === 'contain' && color === 'black' && 'bg-neutral-900 text-neutral-100 hover:bg-neutral-800',
     variant === 'text' && color === 'black' && 'w-auto h-auto bg-none text-neutral-800 hover:text-neutral-500',
     variant === 'text' && color === 'gray' && 'w-auto h-auto bg-none text-neutral-600 hover:text-neutral-500',
     variant === 'icon' && 'p-1 w-auto h-auto bg-none',
+  )
+  const widthClass = clsx(
+    width === 'default' && 'w-29',
+    width === 'full' && 'w-full',
   )
   const sizeClass = clsx(
     size === 'xs' && 'text-(length:--text100) leading-(--leading100)',
@@ -38,11 +46,11 @@ export function Button({
     size === 'md' && 'text-(length:--text400) leading-(--leading400)',
   )
   const cn = clsx(
-    `flex justify-center items-center
-    w-29 h-10.5
+    `h-10.5
     transition-colors duration-(--duration-button)
     cursor-pointer`,
     variantClass,
+    widthClass,
     sizeClass,
     className,
   )
