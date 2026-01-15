5f562de73f24076798b7071d25fc919289425c7d
ayovu17 <ayovu17@gmail.com>
2026-01-15 23:55:22 +0300

    feat: typed footer components

--- src/components/footer/Column.tsx
+++ src/components/footer/Column.tsx
@@ -1,21 +1,17 @@
+import type { Link } from '@/types'
 import { Button, Typography } from '@/components'
-
-interface NavigationColumnItem {
-  label: string
-  to: string
-}
 
 interface NavigationColumnProps {
   title: string
-  list: NavigationColumnItem[]
+  links: Link[]
 }
 
-export function NavigationColumn({ title, list }: NavigationColumnProps) {
+export function NavigationColumn({ title, links }: NavigationColumnProps) {
   return (
     <div className="flex flex-col gap-5">
       <Typography variant="text-100cap" color="gray" tag="span">{title}</Typography>
       <ul className="flex flex-col gap-1">
-        {list.map(i => (
+        {links.map(i => (
           <li key={i.label}>
             <Button variant="text" to={i.to}>{i.label}</Button>
           </li>
