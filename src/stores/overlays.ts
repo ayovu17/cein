c7270b0c5ed80de4a2f80c77701039091e11c7cd
ayovu17 <ayovu17@gmail.com>
2026-01-15 23:28:34 +0300

    feat: add method to close overlay with delay

--- src/stores/overlays.ts
+++ src/stores/overlays.ts
@@ -3,20 +3,42 @@ import { create } from 'zustand'
 
 interface OverlaysStore {
   overlays: Set<Overlay>
+  timer: number | null
+  clearTimer: () => void
   openOverlay: (overlay: Overlay) => void
   closeOverlay: (overlay: Overlay) => void
+  closeOverlayWithDelay: (overlay: Overlay) => void
 }
 
 export const useOverlaysStore = create<OverlaysStore>()((set, get) => ({
   overlays: new Set(),
+  timer: null,
+  clearTimer: () => {
+    const timer = get().timer
+    if (timer) {
+      clearTimeout(timer)
+      set({ timer: null })
+    }
+  },
   openOverlay: (overlay) => {
+    get().clearTimer()
     const nextSet = new Set(get().overlays)
     nextSet.add(overlay)
     set({ overlays: nextSet })
   },
   closeOverlay: (overlay) => {
+    get().clearTimer()
     const nextSet = new Set(get().overlays)
     nextSet.delete(overlay)
     set({ overlays: nextSet })
+  },
+  closeOverlayWithDelay: (overlay) => {
+    get().clearTimer()
+    const timer = window.setTimeout(() => {
+      const nextSet = new Set(get().overlays)
+      nextSet.delete(overlay)
+      set({ overlays: nextSet, timer: null })
+    }, 300)
+    set({ timer })
   },
 }))
