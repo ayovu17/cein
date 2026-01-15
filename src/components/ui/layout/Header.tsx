19a68842a1105b94192f80b23b4217f1f2000abd
ayovu17 <ayovu17@gmail.com>
2026-01-16 00:11:50 +0300

    feat(ui): add header layout

---
+++ src/components/ui/layout/Header.tsx
@@ -0,0 +1,40 @@
+import clsx from 'clsx'
+import { useEffect, useState } from 'react'
+
+type HeaderLayoutComponent = 'mega-menu' | 'search'
+interface HeaderLayoutProps extends React.ComponentProps<'div'> {
+  component: HeaderLayoutComponent
+  children: React.ReactNode
+}
+
+export function HeaderLayout({ component, children, ...props }: HeaderLayoutProps) {
+  const [isVisible, setIsVisible] = useState(false)
+
+  const componentClass = clsx(
+    component === 'mega-menu' && 'h-[calc(100dvh-94px)] lg:h-auto',
+    component === 'search' && 'h-auto',
+  )
+  const visibilityClass = clsx(isVisible && 'opacity-100')
+  const cn = clsx(
+    `z-(--z-overlay)
+    flex flex-col justify-between
+    absolute top-[94px]
+    w-full
+    bg-neutral-50 border-t border-white-smoke-200
+    opacity-0 overflow-y-auto
+    transition-opacity duration-(--duration-overlay)
+    lg:top-28`,
+    componentClass,
+    visibilityClass,
+  )
+
+  useEffect(() => {
+    setIsVisible(true)
+  }, [])
+
+  return (
+    <div className={cn} {...props}>
+      {children}
+    </div>
+  )
+}
